dd743009751bc58a48c77d29804957230853f152
training <ldthieu121292@gmail.com>
2018-07-19 12:55:51 +0700

    inital project

---
+++ EInvoiceSystem/src/main/java/com/dxc/EInvoiceSystemApplication.java
@@ -0,0 +1,12 @@
+package com.dxc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EInvoiceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EInvoiceSystemApplication.class, args);
+	}
+}
