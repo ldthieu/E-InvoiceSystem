bee16bd896297268d2538283054812d891cfa56d
training <ldthieu121292@gmail.com>
2018-07-19 16:39:32 +0700

    inital project completed

--- EInvoiceSystem/src/main/java/com/dxc/config/DataSeedingListener.java
+++ EInvoiceSystem/src/main/java/com/dxc/config/DataSeedingListener.java
@@ -15,47 +15,49 @@ import com.dxc.repository.UserRepository;
 
 @Component
 public class DataSeedingListener implements ApplicationListener<ContextRefreshedEvent> {
+	
 	@Autowired
-    private UserRepository userRepository;
+	private UserRepository userRepository;
+	
+	@Autowired
+	private RoleRepository roleRepository;
+	
+	@Autowired 
+	private PasswordEncoder passwordEncoder;
+	
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent arg0) {
+		// Roles
+		if (roleRepository.findByName("ROLE_ADMIN") == null) {
+			roleRepository.save(new Role("ROLE_ADMIN"));
+		}
+		
+		if (roleRepository.findByName("ROLE_MEMBER") == null) {
+			roleRepository.save(new Role("ROLE_MEMBER"));
+		}
+		
+		// Admin account
+		if (userRepository.findByUsername("admin@gmail.com") == null) {
+			User admin = new User();
+			admin.setUsername("admin@gmail.com");
+			admin.setPassword(passwordEncoder.encode("123456"));
+			HashSet<Role> roles = new HashSet<>();
+			roles.add(roleRepository.findByName("ROLE_ADMIN"));
+			roles.add(roleRepository.findByName("ROLE_MEMBER"));
+			admin.setRoles(roles);
+			userRepository.save(admin);
+		}
+		
+		// Member account
+		if (userRepository.findByUsername("member@gmail.com") == null) {
+			User user = new User();
+			user.setUsername("member@gmail.com");
+			user.setPassword(passwordEncoder.encode("123456"));
+			HashSet<Role> roles = new HashSet<>();
+			roles.add(roleRepository.findByName("ROLE_MEMBER"));
+			user.setRoles(roles);
+			userRepository.save(user);
+		}
+	}
 
-    @Autowired
-    private RoleRepository roleRepository;
-
-    @Autowired 
-    private PasswordEncoder passwordEncoder;
-
-    @Override
-    public void onApplicationEvent(ContextRefreshedEvent arg0) {
-        // Roles
-        if (roleRepository.findByName("ROLE_ADMIN") == null) {
-            roleRepository.save(new Role("ROLE_ADMIN"));
-        }
-
-        if (roleRepository.findByName("ROLE_MEMBER") == null) {
-            roleRepository.save(new Role("ROLE_MEMBER"));
-        }
-
-        // Admin account
-        if (userRepository.findByUsername("admin@gmail.com") == null) {
-            User admin = new User();
-            admin.setUsername("admin@gmail.com");
-            admin.setPassword(passwordEncoder.encode("123456"));
-            HashSet<Role> roles = new HashSet<>();
-            roles.add(roleRepository.findByName("ROLE_ADMIN"));
-            roles.add(roleRepository.findByName("ROLE_MEMBER"));
-            admin.setRoles(roles);
-            userRepository.save(admin);
-        }
-
-        // Member account
-        if (userRepository.findByUsername("member@gmail.com") == null) {
-            User user = new User();
-            user.setUsername("member@gmail.com");
-            user.setPassword(passwordEncoder.encode("123456"));
-            HashSet<Role> roles = new HashSet<>();
-            roles.add(roleRepository.findByName("ROLE_MEMBER"));
-            user.setRoles(roles);
-            userRepository.save(user);
-        }
-    }
 }
--- EInvoiceSystem/src/main/java/com/dxc/services/UserDetailsServiceImpl.java
+++ EInvoiceSystem/src/main/java/com/dxc/services/UserDetailsServiceImpl.java
@@ -9,12 +9,14 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dxc.models.Role;
 import com.dxc.models.User;
 import com.dxc.repository.UserRepository;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
 	@Autowired
